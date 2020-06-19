cd6b19e9c45cba6c6d17bcb87f51991ebc0d590b
Ciputra Pangestu <ciputrapangestu2810@gmail.com>
2020-06-19 08:00:52 +0200

    committed

---
+++ index1.js
@@ -0,0 +1,6 @@
+import * as fungsi from './index.js';
+
+console.log(fungsi.perkalian(2,4))
+console.log(fungsi.pembagian(8,4))
+console.log(fungsi.penjumlahan(2,4))
+console.log(fungsi.pengurangan(5,4))
